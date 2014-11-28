be82a149d4252228909403833e083543bd026478
martin <gqbfree@msn.com>
2014-11-28 16:47:28 +0800

    generate the right json file

--- douban_rent/douban_rent/pipelines.py
+++ douban_rent/douban_rent/pipelines.py
@@ -5,13 +5,16 @@
 
 import json
 import codecs
+from scrapy.contrib.exporter import JsonLinesItemExporter
 
 class DoubanRentPipeline(object):
     def __init__(self):
         self.file = codecs.open('douban_data_utf8.json', 'wb', encoding='utf-8')
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + '\n'
-        print line
-        self.file.write(line.decode("unicode_escape"))
+        exporter = JsonLinesItemExporter(self.file, encoding='utf-8')
+        exporter.export_item(item)
         return item
+
+    def spider_closed(self, spider):
+        self.file.close()
--- douban_rent/douban_rent/spiders/doubanRent.py
+++ douban_rent/douban_rent/spiders/doubanRent.py
@@ -4,6 +4,7 @@
 from scrapy.selector import Selector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+import HTMLParser
 
 from douban_rent.items import DoubanRentItem
 
@@ -11,6 +12,7 @@ from douban_rent.items import DoubanRentItem
 class doubanRentSpider(CrawlSpider):
     name = "douban_rent"
     start_urls = ["http://www.douban.com/group/shanghaizufang/"]
+    allowed_domains = ["douban.com"]
     rules = [
             Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/shanghaizufang/discussion\?start=\d+'))),
             Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/topic/\d+')), callback="parse_rent"),
@@ -19,6 +21,7 @@ class doubanRentSpider(CrawlSpider):
     def parse_rent(self, response):
         sel  = Selector(response)
         item = DoubanRentItem() 
+        html_parser = HTMLParser.HTMLParser()
 
         item['title'] = sel.xpath('//*[@id="content"]/h1/text()').extract()
         item['link']  = response.url
