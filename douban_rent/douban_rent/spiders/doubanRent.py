16e882ff1bf8a5a35dbc7c699e1a3af56a306aeb
martin <gqbfree@msn.com>
2014-12-12 13:14:35 +0800

    re-install

--- douban_rent/douban_rent/spiders/doubanRent.py
+++ douban_rent/douban_rent/spiders/doubanRent.py
@@ -5,12 +5,8 @@ from scrapy.selector import Selector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 import HTMLParser
-import redis
 
 from douban_rent.items import DoubanRentItem
-
-r = redis.StrictRedis(host='localhost', port='6379', db=0)
-r.set('url_count', 0)
 
 class doubanRentSpider(CrawlSpider):
     name = "douban_rent"
@@ -18,8 +14,14 @@ class doubanRentSpider(CrawlSpider):
     allowed_domains = ["douban.com"]
     rules = [
             Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/shanghaizufang/discussion\?start=\d+'))),
+            #Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/shanghaizufang/discussion\?start=^[\w\W]{0,20}'))),
             Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/topic/\d+')), callback="parse_rent"),
     ]
+
+    def __del__(self):
+        with open('/home/qingbo_gao/martin/scrapy/project/douban_rent/info.txt', 'w') as f:
+            f.write(self.state['url_count'])
+    
 
     def parse_rent(self, response):
         sel  = Selector(response)
@@ -31,4 +33,5 @@ class doubanRentSpider(CrawlSpider):
         item['desc']  = "".join(sel.xpath('//*[@id="link-report"]/div/p/text()').extract())
 
         self.state['url_count'] = self.state.get('url_count', 0) + 1
+
         return item
